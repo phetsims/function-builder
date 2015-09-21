c565f6bda700e8dd06acb648ccd3e7ff97dce5c7
Chris Malley <cmalley@pixelzoom.com>
2015-09-21 14:38:28 -0600

    doc

--- js/common/model/FBFunction.js
+++ js/common/model/FBFunction.js
@@ -1,7 +1,8 @@
 // Copyright 2002-2015, University of Colorado Boulder
 
 /**
- * A function. Named FBFunction so that it doesn't conflict with JavaScript's built-in Function type.
+ * A function that takes one input and returns one output.
+ * Named FBFunction so that it doesn't conflict with JavaScript's built-in Function type.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
