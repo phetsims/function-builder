76d7b3d57d3545516c171842991194aa56bb0744
Chris Malley <cmalley@pixelzoom.com>
2015-10-05 21:09:00 -0600

    generate function tests for all cards

--- js/test/view/TestView.js
+++ js/test/view/TestView.js
@@ -11,6 +11,7 @@ define( function( require ) {
   // modules
   var Card = require( 'FUNCTION_BUILDER/common/model/Card' );
   var CardNode = require( 'FUNCTION_BUILDER/common/view/CardNode' );
+  var Carousel = require( 'SUN/Carousel' );
   var Disappear = require( 'FUNCTION_BUILDER/common/model/Disappear' );
   var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
   var Grayscale = require( 'FUNCTION_BUILDER/common/model/Grayscale' );
@@ -28,16 +29,42 @@ define( function( require ) {
   var Warhol = require( 'FUNCTION_BUILDER/common/model/Warhol' );
 
   // images
+  // input card images
+  var beakerImage = require( 'image!FUNCTION_BUILDER/inputs/beaker.png' );
+  var butterflyImage = require( 'image!FUNCTION_BUILDER/inputs/butterfly.png' );
   var cherriesImage = require( 'image!FUNCTION_BUILDER/inputs/cherries.png' );
+  var circleImage = require( 'image!FUNCTION_BUILDER/inputs/circle.png' );
+  var feetImage = require( 'image!FUNCTION_BUILDER/inputs/feet.png' );
+  var planetImage = require( 'image!FUNCTION_BUILDER/inputs/planet.png' );
+  var rectangleImage = require( 'image!FUNCTION_BUILDER/inputs/rectangle.png' );
+  var snowflakeImage = require( 'image!FUNCTION_BUILDER/inputs/snowflake.png' );
+  var starImage = require( 'image!FUNCTION_BUILDER/inputs/star.png' );
+  var stickFigureImage = require( 'image!FUNCTION_BUILDER/inputs/stickFigure.png' );
+  var sunImage = require( 'image!FUNCTION_BUILDER/inputs/sun.png' );
+  var triangleImage = require( 'image!FUNCTION_BUILDER/inputs/triangle.png' );
 
   /**
    * @constructor
    */
   function TestView() {
 
+    var thisView = this;
     ScreenView.call( this, FBConstants.SCREEN_VIEW_OPTIONS );
 
-    var inputCard = new Card( 'cherries', cherriesImage );
+    var cards = [
+      new Card( 'feet', feetImage ),
+      new Card( 'snowflake', snowflakeImage ),
+      new Card( 'butterfly', butterflyImage ),
+      new Card( 'stick-figure', stickFigureImage ),
+      new Card( 'planet', planetImage ),
+      new Card( 'sun', sunImage ),
+      new Card( 'beaker', beakerImage ),
+      new Card( 'cherries', cherriesImage ),
+      new Card( 'rectangle', rectangleImage ),
+      new Card( 'circle', circleImage ),
+      new Card( 'triangle', triangleImage ),
+      new Card( 'star', starImage )
+    ];
 
     var functions = [
       new Disappear(),
@@ -53,17 +80,35 @@ define( function( require ) {
       new Warhol()
     ];
 
-    var boxChildren = [ new CardNode( inputCard ) ];
-    functions.forEach( function( functionInstance ) {
-      var outputCard = functionInstance.apply( inputCard );
-      boxChildren.push( new CardNode( outputCard ) );
+    var items = [];
+
+    // A row for each card
+    cards.forEach( function( card ) {
+
+      var hBoxChildren = [ new CardNode( card ) ];
+
+      functions.forEach( function( functionInstance ) {
+        var outputCard = functionInstance.apply( card );
+        hBoxChildren.push( new CardNode( outputCard ) );
+      } );
+
+      items.push( new HBox( {
+        children: hBoxChildren,
+        spacing: 15,
+        center: thisView.layoutBounds.center
+      } ) );
     } );
 
-    this.addChild( new HBox( {
-      children: boxChildren,
-      spacing: 15,
+    var carousel = new Carousel( items, {
+      orientation: 'vertical',
+      pageControlVisible: true,
+      pageControlLocation: 'right',
+      separatorsVisible: true,
+      itemsPerPage: 6,
       center: this.layoutBounds.center
-    } ) );
+    } );
+
+    this.addChild( carousel );
   }
 
   return inherit( ScreenView, TestView );
