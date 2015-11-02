b5611423df7dc9b0fa62ba305fa2b9fd30d11eb8
Chris Malley <cmalley@pixelzoom.com>
2015-11-02 15:28:32 -0700

    invert only non-transparent pixels

--- js/common/model/InvertRGB.js
+++ js/common/model/InvertRGB.js
@@ -44,13 +44,15 @@ define( function( require ) {
 
       var imageData = card.getImageData();
 
-      // invert colors
+      // invert colors for non-transparent pixels
       var data = imageData.data;
       for ( var i = 0; i < data.length; i += 4 ) {
-        data[ i ] = 255 - data[ i ];
-        data[ i + 1 ] = 255 - data[ i + 1 ];
-        data[ i + 2 ] = 255 - data[ i + 2 ];
-        data[ i + 3 ] = 255;
+        if ( data[ i + 3 ] !== 0 ) {
+          data[ i ] = 255 - data[ i ];
+          data[ i + 1 ] = 255 - data[ i + 1 ];
+          data[ i + 2 ] = 255 - data[ i + 2 ];
+          data[ i + 3 ] = 255;
+        }
       }
 
       return Card.withImageData( card.name + '.' + this.name, imageData );
