ec96d606d13601291d9cb08224635a0cd22ed9ab
Chris Malley <cmalley@pixelzoom.com>
2015-12-16 14:38:13 -0700

    TODO

--- js/test/view/testFunctionInteractions.js
+++ js/test/view/testFunctionInteractions.js
@@ -74,6 +74,7 @@ define( function( require ) {
 
   functionBuilder.register( 'testFunctionInteractions', testFunctionInteractions );
 
+  //TODO Carousel subtype needed because items require a reference to the carousel. Can this reference be eliminated?
   /**
    * @param {function} functionConstructors - constructors of type {AbstractFunction}
    * @param {Node} functionsParentNode
