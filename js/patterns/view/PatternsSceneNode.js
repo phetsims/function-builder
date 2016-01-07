8f04f99c3b969468b44220eab7426b7a26201cb6
Chris Malley <cmalley@pixelzoom.com>
2016-01-07 15:45:15 -0700

    move eraser button a little further away from carousel's next button

--- js/patterns/view/PatternsSceneNode.js
+++ js/patterns/view/PatternsSceneNode.js
@@ -93,7 +93,7 @@ define( function( require ) {
     var eraserButton = new EraserButton( {
       iconWidth: 28,
       centerX: outputCarouselsParent.centerX,
-      top: outputCarouselsParent.bottom + 30
+      top: outputCarouselsParent.bottom + 40
     } );
 
     // parent node for all nodes that are dynamically created
