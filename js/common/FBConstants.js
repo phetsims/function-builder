8f4e16d7fd421ecf8a3914b3a3390eeaed65126f
Chris Malley <cmalley@pixelzoom.com>
2016-01-18 12:10:23 -0700

    doc

--- js/common/FBConstants.js
+++ js/common/FBConstants.js
@@ -12,10 +12,10 @@ define( function( require ) {
 
     SCREEN_VIEW_OPTIONS: { layoutBounds: new Bounds2( 0, 0, 1024, 618 ) },
 
-    // width of a function piece, height is computed so that aspect ratio remains the same at all sizes
+    // width of a function's shape (height is computed so that aspect ratio remains the same at all sizes)
     FUNCTION_WIDTH: 120,
 
-    // x-inset of arrow-like ends of the function shape
+    // x-inset of arrow-like ends of a function's shape
     FUNCTION_X_INSET_FACTOR: 0.15,
 
     // offset used when functions and cards are 'popped out' of their container (carousel or builder)
