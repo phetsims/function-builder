9f73127aa77161a54dd7986de1c042cbdb538341
Chris Malley <cmalley@pixelzoom.com>
2016-01-27 17:07:17 -0700

    icon for Patterns screen (looks a little crunchy), #6

--- js/patterns/PatternsScreen.js
+++ js/patterns/PatternsScreen.js
@@ -11,18 +11,30 @@ define( function( require ) {
   // modules
   var FBColors = require( 'FUNCTION_BUILDER/common/FBColors' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
+  var Image = require( 'SCENERY/nodes/Image' );
+  var ImageCard = require( 'FUNCTION_BUILDER/patterns/model/ImageCard' );
   var inherit = require( 'PHET_CORE/inherit' );
   var PatternsModel = require( 'FUNCTION_BUILDER/patterns/model/PatternsModel' );
   var PatternsView = require( 'FUNCTION_BUILDER/patterns/view/PatternsView' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Screen = require( 'JOIST/Screen' );
+  var ScreenIcon = require( 'JOIST/ScreenIcon' );
+  var Warhol = require( 'FUNCTION_BUILDER/patterns/model/functions/Warhol' );
+
+  // images
+  var starImage = require( 'image!FUNCTION_BUILDER/inputs/star.png' );
 
   // strings
   var screenPatternsString = require( 'string!FUNCTION_BUILDER/screen.patterns' );
 
-  //TODO creates the icon for this screen
+  /**
+   * Creates the icon for this screen, the Warhol function applied to the star image.
+   * @returns {Node}
+   */
   var createIcon = function() {
-    return new Rectangle( 0, 0, Screen.HOME_SCREEN_ICON_SIZE.width, Screen.HOME_SCREEN_ICON_SIZE.height, { fill: 'white' } );
+    var functionInstance = new Warhol();
+    var card = ImageCard.withImage( starImage );
+    var imageNode = new Image( functionInstance.apply( card.canvas ) );
+    return new ScreenIcon( imageNode, { fill: 'rgb( 255, 247, 235 )' } );
   };
 
   /**
