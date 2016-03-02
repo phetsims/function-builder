0fe411427d0cd51fd726b3d1c376a536d106a35f
Chris Malley <cmalley@pixelzoom.com>
2016-03-02 12:52:13 -0700

    move common interactions to FunctionNode base type

--- js/common/view/FunctionNode.js
+++ js/common/view/FunctionNode.js
@@ -10,20 +10,130 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var MovableNode = require( 'FUNCTION_BUILDER/common/view/MovableNode' );
 
   /**
-   * @param {AbstractFunction} functionInstance
+   * @param {ImageFunction} functionInstance
+   * @param {ImageFunctionContainer} container
+   * @param {BuilderNode} builderNode
+   * @param {Node} worldNode
    * @param {Object} [options]
    * @constructor
    */
-  function FunctionNode( functionInstance, options ) {
+  function FunctionNode( functionInstance, container, builderNode, worldNode, options ) {
+
+    options = options || {};
+
+    var thisNode = this;
+
+    // @private
     this.functionInstance = functionInstance;
+    this.container = container;
+    this.builderNode = builderNode;
+    this.worldNode = worldNode;
+
+    assert && assert( !options.startDrag );
+    options.startDrag = function( event, trail ) {
+
+      if ( container.containsNode( thisNode ) ) {
+
+        // function is in the carousel, pop it out
+        container.removeNode( thisNode );
+        worldNode.addChild( thisNode );
+        functionInstance.moveTo( container.carouselLocation.plus( FBConstants.FUNCTION_POP_OUT_OFFSET ) );
+      }
+      else if ( builderNode.containsFunctionNode( thisNode ) ) {
+
+        // function is in the builder, pop it out
+        var slotNumber = builderNode.getSlotNumber( thisNode );
+        var slotLocation = builderNode.getSlotLocation( slotNumber );
+        builderNode.removeFunctionNode( thisNode, slotNumber );
+        worldNode.addChild( thisNode );
+        functionInstance.moveTo( slotLocation.plus( FBConstants.FUNCTION_POP_OUT_OFFSET ) );
+      }
+      else {
+        // function was grabbed while in the world, do nothing
+      }
+
+      assert && assert( worldNode.hasChild( thisNode ) );
+    };
+
+    assert && assert( !options.endDrag );
+    options.endDrag = function( event, trail ) {
+
+      // Find the closest slot
+      var slotNumber = builderNode.getClosestSlot( functionInstance.locationProperty.get() );
+
+      if ( slotNumber === -1 ) {
+
+        // return function to the carousel
+        thisNode.returnToCarousel();
+      }
+      else {
+
+        // put function in builder slot
+        functionInstance.animateTo( builderNode.getSlotLocation( slotNumber ),
+          function() {
+
+            //TODO if an adjacent slot is empty, move the occupying function there
+            // If the slot is occupied, return the occupying function to the carousel.
+            var occupierNode = builderNode.getFunctionNode( slotNumber );
+            occupierNode && occupierNode.returnToCarousel();
+
+            worldNode.removeChild( thisNode );
+            builderNode.addFunctionNode( thisNode, slotNumber );
+          } );
+      }
+    };
+
     MovableNode.call( this, functionInstance, options );
   }
 
   functionBuilder.register( 'FunctionNode', FunctionNode );
 
-  return inherit( MovableNode, FunctionNode );
+  return inherit( MovableNode, FunctionNode, {
+
+    /**
+     * Returns this function to the carousel.
+     *
+     * @param {Object} options
+     * @public
+     */
+    returnToCarousel: function( options ) {
+
+      options = _.extend( {
+        animate: true // true: animate back to carousel, false: move immediate back to carousel
+      }, options );
+
+      if ( !this.container.containsNode( this ) ) {
+
+        // if in the builder, move to the world
+        if ( this.builderNode.containsFunctionNode( this ) ) {
+          var slotNumber = this.builderNode.getSlotNumber( this );
+          this.builderNode.removeFunctionNode( this, slotNumber );
+          this.worldNode.addChild( this );
+        }
+
+        if ( options.animate ) {
+
+          // animate to the carousel
+          var thisNode = this;
+          this.functionInstance.animateTo( this.container.carouselLocation,
+            function() {
+              thisNode.worldNode.removeChild( thisNode );
+              thisNode.container.addNode( thisNode );
+            } );
+        }
+        else {
+
+          // move immediately to the carousel
+          this.worldNode.removeChild( this );
+          this.functionInstance.moveTo( this.container.carouselLocation );
+          this.container.addNode( this );
+        }
+      }
+    }
+  } );
 } );
--- js/patterns/view/ImageFunctionNode.js
+++ js/patterns/view/ImageFunctionNode.js
@@ -9,7 +9,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
   var FunctionBackgroundNode = require( 'FUNCTION_BUILDER/common/view/FunctionBackgroundNode' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var FunctionNode = require( 'FUNCTION_BUILDER/common/view/FunctionNode' );
@@ -33,10 +32,6 @@ define( function( require ) {
       iconScale: 0.3 // {number} scale for icon
     }, options );
 
-    this.container = container; // @public
-
-    var thisNode = this;
-
     var backgroundNode = new FunctionBackgroundNode( functionInstance.viewInfo );
 
     var iconNode = new Image( functionInstance.image, {
@@ -47,110 +42,10 @@ define( function( require ) {
     assert && assert( !options.children, 'decoration not supported' );
     options.children = [ backgroundNode, iconNode ];
 
-    assert && assert( !options.startDrag );
-    options.startDrag = function( event, trail ) {
-
-      if ( container.containsNode( thisNode ) ) {
-
-        // function is in the carousel, pop it out
-        container.removeNode( thisNode );
-        worldNode.addChild( thisNode );
-        functionInstance.moveTo( container.carouselLocation.plus( FBConstants.FUNCTION_POP_OUT_OFFSET ) );
-      }
-      else if ( builderNode.containsFunctionNode( thisNode ) ) {
-
-        // function is in the builder, pop it out
-        var slotNumber = builderNode.getSlotNumber( thisNode );
-        var slotLocation = builderNode.getSlotLocation( slotNumber );
-        builderNode.removeFunctionNode( thisNode, slotNumber );
-        worldNode.addChild( thisNode );
-        functionInstance.moveTo( slotLocation.plus( FBConstants.FUNCTION_POP_OUT_OFFSET ) );
-      }
-      else {
-        // function was grabbed while in the world, do nothing
-      }
-
-      assert && assert( worldNode.hasChild( thisNode ) );
-    };
-
-    assert && assert( !options.endDrag );
-    options.endDrag = function( event, trail ) {
-
-      // Find the closest slot
-      var slotNumber = builderNode.getClosestSlot( functionInstance.locationProperty.get() );
-
-      if ( slotNumber === -1 ) {
-
-        // return function to the carousel
-        thisNode._returnToCarousel();
-      }
-      else {
-
-        // put function in builder slot
-        functionInstance.animateTo( builderNode.getSlotLocation( slotNumber ),
-          function() {
-
-            //TODO if an adjacent slot is empty, move the occupying function there
-            // If the slot is occupied, return the occupying function to the carousel.
-            var occupierNode = builderNode.getFunctionNode( slotNumber );
-            occupierNode && occupierNode.returnToCarousel();
-
-            worldNode.removeChild( thisNode );
-            builderNode.addFunctionNode( thisNode, slotNumber );
-          } );
-      }
-    };
-
-    // @private
-    this._returnToCarousel = function( options ) {
-
-      options = _.extend( {
-        animate: true // true: animate back to carousel, false: move immediate back to carousel
-      }, options );
-
-      if ( !container.containsNode( thisNode ) ) {
-
-        // if in the builder, move to the world
-        if ( builderNode.containsFunctionNode( thisNode ) ) {
-          var slotNumber = builderNode.getSlotNumber( thisNode );
-          builderNode.removeFunctionNode( thisNode, slotNumber );
-          worldNode.addChild( thisNode );
-        }
-
-        if ( options.animate ) {
-
-          // animate to the carousel
-          thisNode.functionInstance.animateTo( thisNode.container.carouselLocation,
-            function() {
-              worldNode.removeChild( thisNode );
-              thisNode.container.addNode( thisNode );
-            } );
-        }
-        else {
-
-          // move immediately to the carousel
-          worldNode.removeChild( thisNode );
-          thisNode.functionInstance.moveTo( thisNode.container.carouselLocation );
-          thisNode.container.addNode( thisNode );
-        }
-      }
-    };
-
-    FunctionNode.call( this, functionInstance, options );
+    FunctionNode.call( this, functionInstance, container, builderNode, worldNode, options );
   }
 
   functionBuilder.register( 'ImageFunctionNode', ImageFunctionNode );
 
-  return inherit( FunctionNode, ImageFunctionNode, {
-
-    /**
-     * Returns this function to the carousel.
-     *
-     * @param {Object} options see _returnToCarousel
-     * @public
-     */
-    returnToCarousel: function( options ) {
-      this._returnToCarousel( options );
-    }
-  } );
+  return inherit( FunctionNode, ImageFunctionNode );
 } );
