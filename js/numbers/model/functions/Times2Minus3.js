bbd325f030bd0bfa4098b2931fc799d8c9169772
Chris Malley <cmalley@pixelzoom.com>
2016-03-22 18:29:10 -0600

    change temporary separator in double function labels to ','

--- js/numbers/model/functions/Times2Minus3.js
+++ js/numbers/model/functions/Times2Minus3.js
@@ -21,7 +21,7 @@ define( function( require ) {
     options = _.extend( {}, options, {
       fill: 'rgb( 165, 209, 167 )' //TODO double function, other half has fill: 'rgb( 255, 246, 187 )'
     } );
-    NumberFunction.call( this, '? ? | ? ?', options );
+    NumberFunction.call( this, '? ? , ? ?', options );
   }
 
   functionBuilder.register( 'Times2Minus3', Times2Minus3 );
