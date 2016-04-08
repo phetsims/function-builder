299574c6c44d72f63dd36c906a51789e14716894
Chris Malley <cmalley@pixelzoom.com>
2016-04-08 12:37:44 -0600

    #14 brighten up background in rightBottom quadrant by tweaking color map

--- js/patterns/model/functions/Warhol.js
+++ js/patterns/model/functions/Warhol.js
@@ -44,7 +44,7 @@ define( function( require ) {
   var LEFT_TOP_COLOR_MAP = [ new Color( 0, 0, 255 ), new Color( 0, 255, 0 ), new Color( 255, 0, 0 ), new Color( 255, 255, 0 ) ];
   var RIGHT_TOP_COLOR_MAP = [ new Color( 0, 100, 255 ), new Color( 165, 255, 0 ), new Color( 255, 0, 132 ), new Color( 255, 215, 140 ) ];
   var LEFT_BOTTOM_COLOR_MAP = [ new Color( 19, 31, 24 ), new Color( 76, 76, 76 ), new Color( 65, 0, 89 ), new Color( 255, 125, 18 ) ];
-  var RIGHT_BOTTOM_COLOR_MAP = [ new Color( 145, 132, 98 ), new Color( 184, 45, 63 ), new Color( 25, 78, 125 ), new Color( 25, 25, 47 ) ];
+  var RIGHT_BOTTOM_COLOR_MAP = [ new Color( 145, 132, 98 ), new Color( 184, 45, 63 ), new Color( 25, 78, 125 ), new Color( 55, 211, 37 ) ];
 
   //TODO delete this when we make a decision
   var OPAQUE_BACKGROUND = true; // should the background of the image be made opaque?
