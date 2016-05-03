707f699e5c7ae49ac1d243fb723fe4a9c0542962
Chris Malley <cmalley@pixelzoom.com>
2016-05-03 14:52:29 -0600

    add model-view transform to graph

--- js/equations/view/XYGraphNode.js
+++ js/equations/view/XYGraphNode.js
@@ -1,6 +1,5 @@
 // Copyright 2016, University of Colorado Boulder
 
-//TODO graph should use a ModelViewTransform2
 /**
  * XY graph for the 'Equations' screen.
  *
@@ -15,17 +14,17 @@ define( function( require ) {
   var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Range = require( 'DOT/Range' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Shape = require( 'KITE/Shape' );
+  var Vector2 = require( 'DOT/Vector2' );
 
   // constants
   var POINT_RADIUS = 3;
   var POINT_FILL = 'black';
-  var X_AXIS_RANGE = new Range( -5, 15 );
-  var Y_AXIS_RANGE = new Range( -5, 15 );
   var AXIS_OPTIONS = {
     doubleHead: true,
     headWidth: 8,
@@ -42,8 +41,21 @@ define( function( require ) {
   function XYGraphNode( options ) {
 
     options = _.extend( {
-      size: FBConstants.GRAPH_DRAWER_SIZE
+      size: FBConstants.GRAPH_DRAWER_SIZE,
+      xRange: new Range( -100, 100 ),
+      yRange: new Range( -100, 100 ),
+      xGridSpacing: 10,
+      yGridSpacing: 10,
+      xTickSpacing: 50,
+      yTickSpacing: 50
     }, options );
+
+    // model-view transform
+    var xOffset = ( 1 - options.xRange.max / options.xRange.getLength() ) * options.size.width;
+    var yOffset = ( 1 - options.yRange.max / options.yRange.getLength() ) * options.size.height;
+    var xScale = options.size.width / options.xRange.getLength();
+    var yScale = -options.size.height / options.yRange.getLength(); // inverted
+    var modelViewTransform = ModelViewTransform2.createOffsetXYScaleMapping( new Vector2( xOffset, yOffset ), xScale, yScale );
 
     var backgroundNode = new Rectangle( 0, 0, options.size.width, options.size.height, {
       fill: 'white'
@@ -52,18 +64,20 @@ define( function( require ) {
     // grid
     var gridShape = new Shape();
 
-    // horizontal lines
-    var Y_GRID_SPACING = backgroundNode.height / Y_AXIS_RANGE.getLength();
-    for ( var row = 1; row < Y_AXIS_RANGE.getLength(); row++ ) {
-      gridShape.moveTo( 0, row * Y_GRID_SPACING );
-      gridShape.lineTo( backgroundNode.width, row * Y_GRID_SPACING );
+    // vertical lines
+    for ( var x = options.xRange.min + options.xGridSpacing; x < options.xRange.max; ) {
+      var viewX = modelViewTransform.modelToViewX( x );
+      gridShape.moveTo( viewX, 0 );
+      gridShape.lineTo( viewX, backgroundNode.height );
+      x += options.xGridSpacing;
     }
 
-    // vertical lines
-    var X_GRID_SPACING = backgroundNode.width / X_AXIS_RANGE.getLength();
-    for ( var column = 1; column < X_AXIS_RANGE.getLength(); column++ ) {
-      gridShape.moveTo( column * X_GRID_SPACING, 0 );
-      gridShape.lineTo( column * X_GRID_SPACING, backgroundNode.height );
+    // horizontal lines
+    for ( var y = options.yRange.min + options.yGridSpacing; y < options.yRange.max; ) {
+      var viewY = modelViewTransform.modelToViewY( y );
+      gridShape.moveTo( 0, viewY );
+      gridShape.lineTo( backgroundNode.width, viewY );
+      y += options.yGridSpacing;
     }
 
     var gridNode = new Path( gridShape, {
@@ -71,36 +85,31 @@ define( function( require ) {
       lineWidth: 0.5
     } );
 
+    var viewOrigin = modelViewTransform.modelToViewXY( 0, 0 );
+
     // x axis
     var xAxisNode = new ArrowNode( 0, 0, backgroundNode.width, 0, AXIS_OPTIONS );
-    xAxisNode.centerY = ( X_AXIS_RANGE.max / X_AXIS_RANGE.getLength() ) * backgroundNode.height;
+    xAxisNode.centerY = viewOrigin.y;
 
     // y axis
     var yAxisNode = new ArrowNode( 0, 0, 0, backgroundNode.height, AXIS_OPTIONS );
-    yAxisNode.centerX = ( 1 - ( Y_AXIS_RANGE.max / Y_AXIS_RANGE.getLength() ) ) * backgroundNode.width;
+    yAxisNode.centerX = viewOrigin.x;
 
     //TODO temporary, demonstrate a few points
     var pointsParent = new Node();
-    pointsParent.addChild( new Circle( POINT_RADIUS, {
-      fill: POINT_FILL,
-      x: 5 * X_GRID_SPACING,
-      y: 10 * Y_GRID_SPACING
-    } ) );
-    pointsParent.addChild( new Circle( POINT_RADIUS, {
-      fill: POINT_FILL,
-      x: 6 * X_GRID_SPACING,
-      y: 10 * Y_GRID_SPACING
-    } ) );
-    pointsParent.addChild( new Circle( POINT_RADIUS, {
-      fill: POINT_FILL,
-      x: 8 * X_GRID_SPACING,
-      y: 10 * Y_GRID_SPACING
-    } ) );
-    pointsParent.addChild( new Circle( POINT_RADIUS, {
-      fill: POINT_FILL,
-      x: 12 * X_GRID_SPACING,
-      y: 10 * Y_GRID_SPACING
-    } ) );
+    var points = [
+      new Vector2( 0, 0 ),
+      new Vector2( 20, 20 ),
+      new Vector2( 40, 40 ),
+      new Vector2( 60, 60 ),
+      new Vector2( 80, 80 )
+    ];
+    points.forEach( function( point ) {
+      pointsParent.addChild( new Circle( POINT_RADIUS, {
+        fill: POINT_FILL,
+        center: modelViewTransform.modelToViewPosition( point )
+      } ) );
+    } );
 
     assert && assert( !options.children, 'decoration not supported' );
     options.children = [ backgroundNode, gridNode, xAxisNode, yAxisNode, pointsParent ];
