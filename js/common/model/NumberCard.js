4a665370b893ede72eb693131302c3a23ea2193f
Chris Malley <cmalley@pixelzoom.com>
2016-05-06 15:26:36 -0600

    doc

--- js/common/model/NumberCard.js
+++ js/common/model/NumberCard.js
@@ -1,7 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- * An input card with a number on it.
+ * An input card with a rational number on it.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
