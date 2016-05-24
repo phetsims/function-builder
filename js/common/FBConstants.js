459d874f8d548e923fe666103b51fde188187607
Chris Malley <cmalley@pixelzoom.com>
2016-05-24 11:26:10 -0600

    delete unused constants

--- js/common/FBConstants.js
+++ js/common/FBConstants.js
@@ -61,10 +61,6 @@ define( function( require ) {
     NUMBERS_CARD_WHOLE_NUMBER_FONT: new FBFont( 30 ),
     NUMBERS_CARD_FRACTION_FONT: new FBFont( 20 ),
 
-    // fonts for carousel labels
-    NUMBERS_CARD_CAROUSEL_LABEL_FONT: new FBFont( 20 ),
-    EQUATIONS_CARD_CAROUSEL_LABEL_FONT: new FBFont( 30 ),
-
     //--------------------------------------------------------------------------------------------------------
     // Functions
 
