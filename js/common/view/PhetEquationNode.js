751b43a5cec360076db069f6d8b35a059333ec1f
Chris Malley <cmalley@pixelzoom.com>
2016-05-31 14:36:15 -0600

    add options and lhs to phet-specific equation

--- js/common/view/PhetEquationNode.js
+++ js/common/view/PhetEquationNode.js
@@ -10,7 +10,7 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var FBFont = require( 'FUNCTION_BUILDER/common/FBFont' );
+  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
   var FBSymbols = require( 'FUNCTION_BUILDER/common/FBSymbols' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -26,24 +26,84 @@ define( function( require ) {
 
     options = _.extend( {
 
+      showLeftHandSide: true, // {boolean} whether to show left-hand side of the equation
       xSymbol: FBSymbols.X, // {string} symbol for input
       ySymbol: FBSymbols.Y, // {string} symbol for output
 
       // colors
       xColor: 'black', // {Color|string} for x symbol
       yColor: 'black', // {Color|string} for y symbol
-      color: 'black' // {Color|string} for everything else
+      color: 'black', // {Color|string} for everything else
+
+      // fonts
+      xyFont: FBConstants.EQUATION_CARD_XY_FONT, // {Font} font for x & y symbols
+      symbolFont: FBConstants.EQUATION_CARD_SYMBOL_FONT, // {Font} font for math symbols (equals, plus, minus)
+      numberFont: FBConstants.EQUATION_CARD_WHOLE_NUMBER_FONT, // {Font} font for numbers
+      signFont: FBConstants.EQUATION_CARD_SIGN_FONT, // {Font} font for negative sign
+
+      // fractions
+      fractionScale: 0.67, // {number} how much to scale fractions
+
+      // x spacing
+      equalsXSpacing: 8, // {number} x space on both sides of equals sign
+      signXSpacing: 3, // {number} x spacing between a negative sign and the number that follows it
+      operatorXSpacing: 8, // {number} x space on both sides of an operator
+      integerSlopeXSpacing: 3, // {number} x space between integer slope and x
+      fractionSlopeXSpacing: 6, // {number} x space between fractional slope and x
+
+      // y spacing
+      fractionYSpacing: 2, // {number} y space above and below fraction line
+
+      // y offsets, positive is down, everything is relative to the equals sign
+      xyYOffset: 0, // {number} vertical offset of x & y symbols
+      slopeYOffset: 0, // {number} vertical offset of slope
+      interceptYOffset: 0, // {number} vertical offset of intercept
+      operatorYOffset: 0 // {number} vertical offset of operators (plus, minus)
 
     }, options );
 
     assert && assert( !options.children, 'decoration not supported' );
     options.children = [];
 
-    //TODO temporary
-    var textNode = new Text( FBSymbols.Y + ' = ' + equation.toString(), {
-      font: new FBFont( 20 )
+    // y
+    var yNode = new Text( options.ySymbol, {
+      fill: options.yColor,
+      font: options.xyFont,
+      y: options.xyYOffset
     } );
-    options.children.push( textNode );
+
+    // =
+    var equalsNode = new Text( FBSymbols.EQUALS, {
+      fill: options.color,
+      font: options.symbolFont,
+      left: yNode.right + options.equalsXSpacing
+    } );
+
+    // Create the left-hand side nodes to simplify layout, but add them only if requested
+    if ( options.showLeftHandSide ) {
+      options.children.push( yNode, equalsNode );
+    }
+
+    var mathFunctions = equation.mathFunctions; // {MathFunction[]}
+
+    if ( mathFunctions.length === 0 ) {
+
+      // y = x
+      var xNode = new Text( options.xSymbol, {
+        font: options.xyFont,
+        left: equalsNode.right + options.equalsXSpacing
+      } );
+      options.children.push( xNode );
+    }
+    else {
+
+      //TODO temporary
+      var rhsSideNode = new Text( equation.toString(), {
+        font: options.numberFont,
+        left: equalsNode.right + options.equalsXSpacing
+      } );
+      options.children.push( rhsSideNode );
+    }
 
     Node.call( this, options );
   }
