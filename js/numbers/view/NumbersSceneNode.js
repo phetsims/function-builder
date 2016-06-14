5158fce6862afb8471da9ef9305c23dde5663d38
Chris Malley <cmalley@pixelzoom.com>
2016-06-14 14:57:25 -0600

    factor out NumbersSceneNode

---
+++ js/numbers/view/NumbersSceneNode.js
@@ -0,0 +1,45 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Scene for the 'Numbers' screen, a variation of EquationsScene with different options.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var EquationsSceneNode = require( 'FUNCTION_BUILDER/equations/view/EquationsSceneNode' );
+  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
+  var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  // strings
+  var inputString = require( 'string!FUNCTION_BUILDER/input' );
+  var outputString = require( 'string!FUNCTION_BUILDER/output' );
+
+  /**
+   * @param {Scene} scene - model for this scene
+   * @param {Bounds2} layoutBounds - layoutBounds of the parent ScreenView
+   * @param {Object} [options]
+   * @constructor
+   */
+  function NumbersSceneNode( scene, layoutBounds, options ) {
+
+    options = _.extend( {
+      functionsPerPage: 3,
+      hasGraph: false, // no graph in this scene
+      xSymbol: inputString, // symbol used in place of x
+      ySymbol: outputString, // symbol used in place of y
+      xyFont: FBConstants.EQUATION_CARD_INPUT_OUTPUT_FONT, // {Font} for x & y symbols
+      xyAsCardsInEquations: true, // {boolean} make x & y symbols look like a card in equations
+      tableHeadingFont: FBConstants.TABLE_INPUT_OUTPUT_HEADING_FONT
+    }, options );
+
+    EquationsSceneNode.call( this, scene, layoutBounds, options );
+  }
+
+  functionBuilder.register( 'NumbersSceneNode', NumbersSceneNode );
+
+  return inherit( EquationsSceneNode, NumbersSceneNode );
+} );
--- js/numbers/view/NumbersScreenView.js
+++ js/numbers/view/NumbersScreenView.js
@@ -1,7 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- * ScreenView for the 'Numbers' screen, a variation of the 'Equations' screen.
+ * ScreenView for the 'Numbers' screen.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -9,27 +9,10 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var EquationsSceneNode = require( 'FUNCTION_BUILDER/equations/view/EquationsSceneNode' );
-  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
   var FBScreenView = require( 'FUNCTION_BUILDER/common/view/FBScreenView' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var inherit = require( 'PHET_CORE/inherit' );
-
-  // strings
-  var inputString = require( 'string!FUNCTION_BUILDER/input' );
-  var outputString = require( 'string!FUNCTION_BUILDER/output' );
-
-  // constants
-  var SCENE_NODE_OPTIONS = {
-    cardCarouselDefaultPageNumber: 1,
-    functionsPerPage: 3,
-    hasGraph: false, // no graph in this scene
-    xSymbol: inputString, // symbol used in place of x
-    ySymbol: outputString, // symbol used in place of y
-    xyFont: FBConstants.EQUATION_CARD_INPUT_OUTPUT_FONT, // {Font} for x & y symbols
-    xyAsCardsInEquations: true, // {boolean} make x & y symbols look like a card in equations
-    tableHeadingFont: FBConstants.TABLE_INPUT_OUTPUT_HEADING_FONT
-  };
+  var NumbersSceneNode = require( 'FUNCTION_BUILDER/numbers/view/NumbersSceneNode' );
 
   /**
    * @param {NumbersModel} model
@@ -55,7 +38,7 @@ define( function( require ) {
      * @override
      */
     createSceneNode: function( scene, layoutBounds, options ) {
-      return new EquationsSceneNode( scene, layoutBounds, _.extend( {}, SCENE_NODE_OPTIONS, options ) );
+      return new NumbersSceneNode( scene, layoutBounds, options );
     }
   } );
 } );
