7b6f711c1c8ec1c2726671077ab5a6d98a79143b
Chris Malley <cmalley@pixelzoom.com>
2016-06-14 12:10:46 -0600

    move a bunch of stuff into MysteryScene

--- js/mystery/model/MysteryModel.js
+++ js/mystery/model/MysteryModel.js
@@ -9,153 +9,25 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
-  var FunctionCreator = require( 'FUNCTION_BUILDER/common/model/functions/FunctionCreator' );
-  var FBIconFactory = require( 'FUNCTION_BUILDER/common/view/FBIconFactory' );
-  var FBSymbols = require( 'FUNCTION_BUILDER/common/FBSymbols' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var MathBuilder = require( 'FUNCTION_BUILDER/common/model/MathBuilder' );
+  var MysteryScene = require( 'FUNCTION_BUILDER/mystery/model/MysteryScene' );
   var Property = require( 'AXON/Property' );
-  var Random = require( 'DOT/Random' );
-  var Range = require( 'DOT/Range' );
-  var RationalNumber = require( 'FUNCTION_BUILDER/common/model/RationalNumber' );
-  var Scene = require( 'FUNCTION_BUILDER/common/model/Scene' );
-  var Vector2 = require( 'DOT/Vector2' );
-
-  // function modules
-  var Divide = require( 'FUNCTION_BUILDER/common/model/functions/Divide' );
-  var Minus = require( 'FUNCTION_BUILDER/common/model/functions/Minus' );
-  var Plus = require( 'FUNCTION_BUILDER/common/model/functions/Plus' );
-  var Times = require( 'FUNCTION_BUILDER/common/model/functions/Times' );
-
-  // constants
-  var BUILDER_WIDTH = ( 3 * FBConstants.FUNCTION_SIZE.width ) + 70;
-  var BUILDER_X = ( FBConstants.SCREEN_VIEW_LAYOUT_BOUNDS.width / 2 ) - ( BUILDER_WIDTH / 2 );
-  var CARD_NUMBERS_RANGE = new Range( -4, 6 );
-  var INCLUDE_X_CARD = false; // whether to include 'x' card in input carousel
 
   /**
    * @constructor
    */
   function MysteryModel() {
 
-    // {RationalNumber[]} rational number cards, in the order that they appear in the carousel
-    var cardContent = [];
-    for ( var i = CARD_NUMBERS_RANGE.min; i <= CARD_NUMBERS_RANGE.max; i++ ) {
-      cardContent.push( RationalNumber.withInteger( i ) );
-    }
-
-    // {FunctionCreator[]} function creators, in the order that functions appear in the carousel
-    var functionCreators = [
-      new FunctionCreator( Plus ),
-      new FunctionCreator( Minus ),
-      new FunctionCreator( Times ),
-      new FunctionCreator( Divide )
-    ];
-
-    // builder for '1 function' scene
-    var builder1 = new MathBuilder( {
-      numberOfSlots: 1,
-      width: BUILDER_WIDTH,
-      location: new Vector2( BUILDER_X, FBConstants.BUILDER_Y )
-    } );
-
-    // builder for '2 functions' scene
-    var builder2 = new MathBuilder( {
-      numberOfSlots: 2,
-      width: BUILDER_WIDTH,
-      location: new Vector2( BUILDER_X, FBConstants.BUILDER_Y )
-    } );
-
-    // builder for '3 functions' scene
-    var builder3 = new MathBuilder( {
-      numberOfSlots: 3,
-      width: BUILDER_WIDTH,
-      location: new Vector2( BUILDER_X, FBConstants.BUILDER_Y )
-    } );
-
-    var cardSymbol = INCLUDE_X_CARD ? FBSymbols.X : null;
-
     // @public (read-only)
     this.scenes = [
-
-      // 1 function scene
-      new Scene( cardContent, functionCreators, builder1, {
-        iconNode: FBIconFactory.createSceneIcon( 1 ),
-        cardSymbol: cardSymbol,
-        numberOfEachCard: 1,
-        numberOfEachFunction: 1
-      } ),
-
-      // 2 functions scene
-      new Scene( cardContent, functionCreators, builder2, {
-        iconNode: FBIconFactory.createSceneIcon( 2 ),
-        cardSymbol: cardSymbol,
-        numberOfEachCard: 1,
-        numberOfEachFunction: 2 //TODO adjust this when I see the actual pool of challenges
-      } ),
-
-      // 3 functions scene
-      new Scene( cardContent, functionCreators, builder3, {
-        iconNode: FBIconFactory.createSceneIcon( 3 ),
-        cardSymbol: cardSymbol,
-        numberOfEachCard: 1,
-        numberOfEachFunction: 3 //TODO adjust this when I see the actual pool of challenges
-      } )
+      new MysteryScene( { functionsPerChallenge: 1 } ),
+      new MysteryScene( { functionsPerChallenge: 2 } ),
+      new MysteryScene( { functionsPerChallenge: 3 } )
     ];
 
     // @public {Property.<Scene>} the selected scene
     this.selectedSceneProperty = new Property( this.scenes[ 0 ] );
-
-    //TODO instantiate these on demand from a description, to use fewer resources and improve readability?
-    /**
-     * Pre-defined pools of challenges, one pool for each scene, indexed by number of functions in the challenge.
-     * The first challenge in each pool is always the first one chose when the sim starts.
-     * @public
-     */
-    this.challengePools = [
-
-      // pool of 1-function challenges
-      [
-        [ new Plus( { operand: 2 } ) ],
-        [ new Minus( { operand: 3 } ) ],
-        [ new Times( { operand: -2 } ) ],
-        [ new Divide( { operand: 2 } ) ]
-      ],
-
-      // pool of 2-function challenges
-      [
-        [ new Plus( { operand: 1 } ), new Times( { operand: 2 } ) ],
-        [ new Times( { operand: 2 } ), new Minus( { operand: 2 } ) ],
-        [ new Times( { operand: -2 } ), new Plus( { operand: 3 } ) ],
-        [ new Minus( { operand: 1 } ), new Divide( { operand: 2 } ) ]
-      ],
-
-      // pool of 3-function challenges
-      [
-        [ new Plus( { operand: 1 } ), new Times( { operand: 2 } ), new Minus( { operand: -1 } ) ],
-        [ new Times( { operand: 2 } ), new Minus( { operand: 2 } ), new Divide( { operand: -1 } ) ],
-        [ new Minus( { operand: 2 } ), new Times( { operand: -2 } ), new Plus( { operand: 3 } ) ],
-        [ new Times( { operand: -1 } ), new Minus( { operand: 2 } ), new Divide( { operand: 3 } ) ]
-      ]
-    ];
-
-    // verify that each challenge has the correct number of functions
-    if ( assert ) {
-      for ( var poolIndex = 0; poolIndex < this.challengePools.length; poolIndex++ ) {
-        var pool = this.challengePools[ poolIndex ];
-        for ( var challengeIndex = 0; challengeIndex < pool.length; challengeIndex++ ) {
-          var challenge = pool[ challengeIndex ];
-          assert( challenge.length === poolIndex + 1,
-            'incorrect number of functions in challenge ' + challengeIndex + ' of pool ' + poolIndex + ': ' + challenge.length );
-        }
-      }
-    }
-
-    //TODO do we need a separate random number generator for each scene?
-    // @private random number generator, for picking challenges
-    this.random = new Random();
   }
 
   functionBuilder.register( 'MysteryModel', MysteryModel );
@@ -180,26 +52,6 @@ define( function( require ) {
       for ( var sceneIndex = 0; sceneIndex < this.scenes.length; sceneIndex++ ) {
         this.scenes[ sceneIndex ].step( dt );
       }
-    },
-
-    /**
-     * Randomly selects a challenge.
-     *
-     * @param {number} numberOfFunctions
-     * @returns {MathFunction[]}
-     */
-    getChallenge: function( numberOfFunctions ) {
-
-      var pool = this.challengePools[ numberOfFunctions - 1 ];
-      assert && assert( pool, 'pool index out of range: ' + ( numberOfFunctions - 1 ) );
-
-      var challengeIndex = this.random.nextInt( pool.length ) - 1;
-      assert && assert( challengeIndex >=0 && challengeIndex < pool.length );
-
-      var challenge = pool[ challengeIndex ];
-      assert && assert( challenge.length === numberOfFunctions );
-
-      return challenge;
     }
   } );
 } );
---
+++ js/mystery/model/MysteryScene.js
@@ -0,0 +1,108 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * A scene in the 'Mystery' screen.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
+  var FBIconFactory = require( 'FUNCTION_BUILDER/common/view/FBIconFactory' );
+  var FBSymbols = require( 'FUNCTION_BUILDER/common/FBSymbols' );
+  var FunctionCreator = require( 'FUNCTION_BUILDER/common/model/functions/FunctionCreator' );
+  var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var MathBuilder = require( 'FUNCTION_BUILDER/common/model/MathBuilder' );
+  var Random = require( 'DOT/Random' );
+  var Range = require( 'DOT/Range' );
+  var RationalNumber = require( 'FUNCTION_BUILDER/common/model/RationalNumber' );
+  var Scene = require( 'FUNCTION_BUILDER/common/model/Scene' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  // function modules
+  var Divide = require( 'FUNCTION_BUILDER/common/model/functions/Divide' );
+  var Minus = require( 'FUNCTION_BUILDER/common/model/functions/Minus' );
+  var Plus = require( 'FUNCTION_BUILDER/common/model/functions/Plus' );
+  var Times = require( 'FUNCTION_BUILDER/common/model/functions/Times' );
+
+  // constants
+  var BUILDER_WIDTH = ( 3 * FBConstants.FUNCTION_SIZE.width ) + 70;
+  var BUILDER_X = ( FBConstants.SCREEN_VIEW_LAYOUT_BOUNDS.width / 2 ) - ( BUILDER_WIDTH / 2 );
+  var CARD_NUMBERS_RANGE = new Range( -4, 6 );
+  var INCLUDE_X_CARD = false; // whether to include 'x' card in input carousel
+
+  /**
+   * @param {Object} [options]
+   * @constructor
+   */
+  function MysteryScene( options ) {
+
+    options = _.extend( {
+      functionsPerChallenge: 1, // {number} number of functions in each challenge
+      numberOfEachCard: 1,
+      numberOfEachFunction: 1,
+      cardSymbol: INCLUDE_X_CARD ? FBSymbols.X : null
+    }, options );
+
+    // {Node} scene selection icon
+    assert && assert( !options.iconNode );
+    options.iconNode = FBIconFactory.createSceneIcon( options.functionsPerChallenge );
+
+    // {RationalNumber[]} rational number cards, in the order that they appear in the carousel
+    var cardContent = [];
+    for ( var i = CARD_NUMBERS_RANGE.min; i <= CARD_NUMBERS_RANGE.max; i++ ) {
+      cardContent.push( RationalNumber.withInteger( i ) );
+    }
+
+    // {FunctionCreator[]} function creators, in the order that functions appear in the carousel
+    var functionCreators = [
+      new FunctionCreator( Plus ),
+      new FunctionCreator( Minus ),
+      new FunctionCreator( Times ),
+      new FunctionCreator( Divide )
+    ];
+
+    var builder = new MathBuilder( {
+      numberOfSlots: options.functionsPerChallenge,
+      width: BUILDER_WIDTH,
+      location: new Vector2( BUILDER_X, FBConstants.BUILDER_Y )
+    } );
+
+    // @private random number generator, for picking challenges
+    this.random = new Random();
+
+    // @private pool of challenges
+    this.pool = []; //TODO populate
+
+    // verify that each challenge contains the correct number of functions
+    if ( assert ) {
+      for ( var challengeIndex = 0; challengeIndex < this.pool.length; challengeIndex++ ) {
+        var challenge = this.pool[ challengeIndex ];
+        assert( challenge.length === options.functionsPerChallenge,
+          'incorrect number of functions in challenge ' + challengeIndex + ': ' + challenge.length );
+      }
+    }
+
+    Scene.call( this, cardContent, functionCreators, builder, options );
+  }
+
+  functionBuilder.register( 'MysteryScene', MysteryScene );
+
+  return inherit( Scene, MysteryScene, {
+
+    /**
+     * Randomly selects a challenge.
+     *
+     * @returns {*} TODO type?
+     */
+    getChallenge: function() {
+      //TODO if this is the first time called, return the first challenge
+      var challengeIndex = this.random.nextInt( this.pool.length );
+      assert && assert( challengeIndex >= 0 && challengeIndex < this.pool.length );
+      return this.pool[ challengeIndex ];
+    }
+  } );
+} );
