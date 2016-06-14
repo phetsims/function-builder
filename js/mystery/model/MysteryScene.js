4741fc9e2da5cc1da48b7c1d64d202464c506ad2
Chris Malley <cmalley@pixelzoom.com>
2016-06-14 16:04:28 -0600

    TODO

--- js/mystery/model/MysteryScene.js
+++ js/mystery/model/MysteryScene.js
@@ -44,7 +44,7 @@ define( function( require ) {
     options = _.extend( {
       functionsPerChallenge: 1, // {number} number of functions in each challenge
       numberOfEachCard: 1,
-      numberOfEachFunction: 1,
+      numberOfEachFunction: 3, //TODO revisit this value, after pools are specified
       cardSymbol: INCLUDE_X_CARD ? FBSymbols.X : null
     }, options );
 
