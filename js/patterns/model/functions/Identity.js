a0dab70b036560066e18ea9904722897d8a69a63
Chris Malley <cmalley@pixelzoom.com>
2016-07-11 08:52:51 -0600

    lint fix, #91

--- js/patterns/model/functions/Identity.js
+++ js/patterns/model/functions/Identity.js
@@ -10,14 +10,10 @@ define( function( require ) {
 
   // modules
   var FBCanvasUtils = require( 'FUNCTION_BUILDER/patterns/model/FBCanvasUtils' );
-  var FBConstants = require( 'FUNCTION_BUILDER/common/FBConstants' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var ImageFunction = require( 'FUNCTION_BUILDER/common/model/functions/ImageFunction' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
-
-  // images
-  var identityImage = require( 'mipmap!FUNCTION_BUILDER/functions/identity.png' );
 
   /**
    * @param {Object} [options]
