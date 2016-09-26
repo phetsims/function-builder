35c24ed5998abb3e353c5a81c4bb03e1da0865e8
Chris Malley <cmalley@pixelzoom.com>
2016-09-26 11:17:20 -0600

    use FontAwesomeNode directly after adding Shape caching, #102

--- js/common/view/EyeCloseNode.js
+++ js/common/view/EyeCloseNode.js
@@ -1,8 +1,9 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- * Node the displays the fontawesome 'eye_close' icon. Reuses 1 Shape instance to improve performance.
- * See https://github.com/phetsims/function-builder/issues/102
+ * Node the displays the fontawesome 'eye_close' icon.
+ * Wrapper around FontAwesomeNode to guard against changes that were made in
+ * https://github.com/phetsims/function-builder/issues/102
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -13,26 +14,16 @@ define( function( require ) {
   var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Path = require( 'SCENERY/nodes/Path' );
-
-  // constants
-  var EYE_CLOSE_SHAPE = FontAwesomeNode.createShape( 'eye_close' );
 
   /**
    * @param {Object} [options]
    * @constructor
    */
   function EyeCloseNode( options ) {
-
-    options = _.extend( {
-      fill: 'black',
-      pickable: false
-    }, options );
-
-    Path.call( this, EYE_CLOSE_SHAPE, options );
+    FontAwesomeNode.call( this, 'eye_close', options );
   }
 
   functionBuilder.register( 'EyeCloseNode', EyeCloseNode );
 
-  return inherit( Path, EyeCloseNode );
+  return inherit( FontAwesomeNode, EyeCloseNode );
 } );
