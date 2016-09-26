98ffbef814301a46f5ba384d57455b73beddfa1c
Chris Malley <cmalley@pixelzoom.com>
2016-09-26 10:57:40 -0600

    reuse 1 instance of 'eye_close' Shape, #102

--- js/common/view/EyeCloseNode.js
+++ js/common/view/EyeCloseNode.js
@@ -1,15 +1,8 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- * Wrapper around a single instance of a FontAwesome( 'eye_closed' ) node.
+ * Node the displays the fontawesome 'eye_close' icon. Reuses 1 Shape instance to improve performance.
  * See https://github.com/phetsims/function-builder/issues/102
- *
- * Parsing the SVG to create a FontAwesomeNode has a significant performance penalty.  Here we reuse a
- * single instance of FontAwesomeNode( 'eye_close' ), and take advantage of scenery's DAG feature, which
- * allows a Node to have multiple parents and therefore appear in the scenegraph more than once.
- * Such a Node should not be transformed directly, so we wrap the FontAwesomeNode instance in a Node.
- * Clients can then instantiate and transform an EyeCloseNode without worrying about the fact that
- * a single FontAwesomeNode instance is reused.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -20,23 +13,26 @@ define( function( require ) {
   var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var functionBuilder = require( 'FUNCTION_BUILDER/functionBuilder' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
 
   // constants
-  var EYE_CLOSE_NODE = new FontAwesomeNode( 'eye_close' );
+  var EYE_CLOSE_SHAPE = FontAwesomeNode.createShape( 'eye_close' );
 
   /**
    * @param {Object} [options]
    * @constructor
    */
   function EyeCloseNode( options ) {
-    options = options || {};
-    assert && assert( !options.children, 'decoration not supported' );
-    options.children = [ EYE_CLOSE_NODE ];
-    Node.call( this, options );
+
+    options = _.extend( {
+      fill: 'black',
+      pickable: false
+    }, options );
+
+    Path.call( this, EYE_CLOSE_SHAPE, options );
   }
 
   functionBuilder.register( 'EyeCloseNode', EyeCloseNode );
 
-  return inherit( Node, EyeCloseNode );
+  return inherit( Path, EyeCloseNode );
 } );
