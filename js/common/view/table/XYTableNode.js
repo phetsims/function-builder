b078aefe134b22ff830cb87a38f7e9cd5ca4692d
phet-dev <phetadmin@gmail.com>
2020-02-04 03:30:47 -0700

    update copyright dates from daily grunt work

--- js/common/view/table/XYTableNode.js
+++ js/common/view/table/XYTableNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * XY table, showing the mapping between input and output values for the functions in the builder.
