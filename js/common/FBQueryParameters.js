de5fc82e725b920a12a09d354d631f3424b05e03
pixelzoom <cmalley@pixelzoom.com>
2020-05-14 14:57:03 -0600

    clean up *QueryParameter.js files for pixelzoom sims, https://github.com/phetsims/aqua/issues/91

--- js/common/FBQueryParameters.js
+++ js/common/FBQueryParameters.js
@@ -10,19 +10,24 @@ import functionBuilder from '../functionBuilder.js';
 
 const FBQueryParameters = QueryStringMachine.getAll( {
 
-  // adds the 'Test' screen
+  // Adds the 'Test' screen.
+  // For internal use only.
   testScreen: { type: 'flag' },
 
-  // populates the output carousel with 1 card of each type
+  // Populates the output carousel with 1 card of each type.
+  // For internal use only.
   populateOutput: { type: 'flag' },
 
-  // puts a red stroke around containers in the carousels, so that empty containers are visible
+  // Puts a red stroke around containers in the carousels, so that empty containers are visible.
+  // For internal use only.
   showContainers: { type: 'flag' },
 
-  // plays all Mystery challenges, in order
+  // Plays all Mystery challenges, in order.
+  // For internal use only.
   playAll: { type: 'flag' },
 
-  // shows all colors, in order that they appear in pool, for Mystery challenges
+  // Shows all colors, in order that they appear in pool, for Mystery challenges.
+  // For internal use only.
   showAllColors: { type: 'flag' }
 } );
 
