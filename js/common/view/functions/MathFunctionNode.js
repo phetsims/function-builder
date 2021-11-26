6b97850ecb8eccd2bcf296cb4f7faf5c89d0a388
phet-dev <phetadmin@gmail.com>
2021-11-26 03:16:54 -0700

    update copyright dates from daily grunt work

--- js/common/view/functions/MathFunctionNode.js
+++ js/common/view/functions/MathFunctionNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2021, University of Colorado Boulder
 
 /**
  * Node that synchronizes with a MathFunction, but does not support editing its operand.
