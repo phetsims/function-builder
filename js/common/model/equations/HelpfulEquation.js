efc87fde4f662e082de297003eb233a2ec59a860
phet-dev <phetadmin@gmail.com>
2022-05-26 03:13:55 -0600

    update copyright dates from daily grunt work

--- js/common/model/equations/HelpfulEquation.js
+++ js/common/model/equations/HelpfulEquation.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * For lack of a better name, this equation format was referred to as the "helpful" format in design meetings.
--- js/common/view/equations/HelpfulEquationNode.js
+++ js/common/view/equations/HelpfulEquationNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * Displays an equation in "helpful" format. See HelpfulEquation for backstory on name and specification.
