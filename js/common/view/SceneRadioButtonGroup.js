785c3d2f345581f267af68a60b92dac415742586
pixelzoom <cmalley@pixelzoom.com>
2022-07-06 16:36:18 -0600

    RectangularRadioButtonGroup: refactor options into nested radioButtonOptions, convert to optionize, fix call sites, https://github.com/phetsims/sun/issues/740

--- js/common/view/SceneRadioButtonGroup.js
+++ js/common/view/SceneRadioButtonGroup.js
@@ -22,10 +22,12 @@ class SceneRadioButtonGroup extends RectangularRadioButtonGroup {
     options = merge( {
       orientation: 'horizontal',
       spacing: 20,
-      baseColor: 'white',
-      selectedLineWidth: 2,
-      buttonContentXMargin: 10,
-      buttonContentYMargin: 16
+      radioButtonOptions: {
+        baseColor: 'white',
+        selectedLineWidth: 2,
+        xMargin: 10,
+        yMargin: 16
+      }
     }, options );
 
     // touchArea optimized for spacing
