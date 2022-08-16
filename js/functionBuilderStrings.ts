97d4191ead130bf375df2617881c705815ff3bab
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:38 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/functionBuilderStrings.ts
+++ js/functionBuilderStrings.ts
@@ -5,27 +5,42 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import functionBuilder from './functionBuilder.js';
 
 type StringsType = {
   'function-builder': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'patterns': string;
+    'patternsProperty': TReadOnlyProperty<string>;
     'numbers': string;
+    'numbersProperty': TReadOnlyProperty<string>;
     'equations': string;
+    'equationsProperty': TReadOnlyProperty<string>;
     'mystery': string;
+    'mysteryProperty': TReadOnlyProperty<string>;
   };
   'simplify': string;
+  'simplifyProperty': TReadOnlyProperty<string>;
   'input': string;
+  'inputProperty': TReadOnlyProperty<string>;
   'output': string;
+  'outputProperty': TReadOnlyProperty<string>;
   'x': string;
+  'xProperty': TReadOnlyProperty<string>;
   'y': string;
+  'yProperty': TReadOnlyProperty<string>;
   'mysteryA': string;
+  'mysteryAProperty': TReadOnlyProperty<string>;
   'mysteryB': string;
+  'mysteryBProperty': TReadOnlyProperty<string>;
   'mysteryC': string;
+  'mysteryCProperty': TReadOnlyProperty<string>;
   'mysteryCharacter': string;
+  'mysteryCharacterProperty': TReadOnlyProperty<string>;
 };
 
 const functionBuilderStrings = getStringModule( 'FUNCTION_BUILDER' ) as StringsType;
