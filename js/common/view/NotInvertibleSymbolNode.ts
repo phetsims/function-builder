2f2ca1d1e85f93c98ea4ecbb227b58ad311b516b
pixelzoom <cmalley@pixelzoom.com>
2023-07-26 16:13:06 -0600

    convert to TypeScript

--- js/common/view/NotInvertibleSymbolNode.ts
+++ js/common/view/NotInvertibleSymbolNode.ts
@@ -9,36 +9,37 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import BannedNode from '../../../../scenery-phet/js/BannedNode.js';
+import BannedNode, { BannedNodeOptions } from '../../../../scenery-phet/js/BannedNode.js';
 import Animation from '../../../../twixt/js/Animation.js';
 import Easing from '../../../../twixt/js/Easing.js';
 import functionBuilder from '../../functionBuilder.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+
+type SelfOptions = EmptySelfOptions;
+
+type NotInvertibleSymbolNodeOptions = SelfOptions & BannedNodeOptions;
 
 export default class NotInvertibleSymbolNode extends BannedNode {
 
-  /**
-   * @param {Object} [options]
-   * @constructor
-   */
-  constructor( options ) {
+  private animation: Animation | null; // animation that fades this Node out
 
-    options = merge( {
+  public constructor( providedOptions?: NotInvertibleSymbolNodeOptions ) {
+
+    const options = optionize<NotInvertibleSymbolNodeOptions, SelfOptions, BannedNodeOptions>()( {
+
+      // BannedNodeOptions
       fill: 'white'
-    }, options );
+    }, providedOptions );
 
     super( options );
 
-    // @private {Animation|null} animation that fades this node out
     this.animation = null;
   }
 
   /**
    * Starts animation.
-   *
-   * @public
    */
-  startAnimation() {
+  public startAnimation(): void {
 
     // stop animation if it's already running
     this.animation && this.animation.stop();
@@ -48,6 +49,7 @@ export default class NotInvertibleSymbolNode extends BannedNode {
       duration: 1.5, // seconds
       easing: Easing.QUADRATIC_IN_OUT,
       object: this,
+      // @ts-expect-error TODO https://github.com/phetsims/twixt/issues/31
       attribute: 'opacity',
       from: 0.85,
       to: 0
@@ -55,7 +57,7 @@ export default class NotInvertibleSymbolNode extends BannedNode {
 
     const endedListener = () => {
       this.visible = false;
-      this.animation.endedEmitter.removeListener( endedListener );
+      this.animation && this.animation.endedEmitter.removeListener( endedListener );
       this.animation = null;
     };
 
@@ -67,10 +69,8 @@ export default class NotInvertibleSymbolNode extends BannedNode {
 
   /**
    * Stops animation. If no animation is in progress, this is a no-op.
-   *
-   * @public
    */
-  stopAnimation() {
+  public stopAnimation(): void {
     if ( this.animation ) {
       this.animation.stop();
       this.visible = false;
