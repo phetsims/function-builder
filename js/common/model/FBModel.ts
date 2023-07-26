55c6856fc6224f71caad0cf9913060ad94b5ac58
pixelzoom <cmalley@pixelzoom.com>
2023-07-26 17:16:37 -0600

    convert to TypeScript

--- js/common/model/FBModel.ts
+++ js/common/model/FBModel.ts
@@ -1,37 +1,42 @@
 // Copyright 2016-2023, University of Colorado Boulder
 
 /**
- * Base class 'model container' for this sim.
+ * FBModel is the base class 'model container' for this sim.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import Property from '../../../../axon/js/Property.js';
-import merge from '../../../../phet-core/js/merge.js';
 import functionBuilder from '../../functionBuilder.js';
+import TModel from '../../../../joist/js/TModel.js';
+import FBScene from './FBScene.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 
-export default class FBModel {
+type SelfOptions = {
+  defaultScene?: FBScene;
+};
 
-  /**
-   * @param {FBScene[]} scenes
-   * @param {Object} [options]
-   */
-  constructor( scenes, options ) {
+export type FBModelOptions = SelfOptions;
 
-    options = merge( {
+export default class FBModel implements TModel {
+
+  public readonly scenes: FBScene[];
+  public readonly selectedSceneProperty: Property<FBScene>; // the selected scene
+
+  protected constructor( scenes: FBScene[], providedOptions?: FBModelOptions ) {
+
+    const options = optionize<FBModelOptions, SelfOptions>()( {
+
+      // SelfOptions
       defaultScene: scenes[ 0 ]
-    }, options );
-    assert && assert( scenes.indexOf( options.defaultScene ) !== -1 );
+    }, providedOptions );
+    assert && assert( scenes.includes( options.defaultScene ) );
 
-    // @public (read-only)
     this.scenes = scenes;
-
-    // @public {Property.<Scene>} the selected scene
     this.selectedSceneProperty = new Property( options.defaultScene );
   }
 
-  // @public
-  reset() {
+  public reset(): void {
     this.selectedSceneProperty.reset();
     for ( let sceneIndex = 0; sceneIndex < this.scenes.length; sceneIndex++ ) {
       this.scenes[ sceneIndex ].reset();
@@ -40,11 +45,9 @@ export default class FBModel {
 
   /**
    * Animates the model.
-   *
-   * @param {number} dt - time since the previous step, in seconds
-   * @public
+   * @param dt - time since the previous step, in seconds
    */
-  step( dt ) {
+  public step( dt: number ): void {
     for ( let sceneIndex = 0; sceneIndex < this.scenes.length; sceneIndex++ ) {
       this.scenes[ sceneIndex ].step( dt );
     }
