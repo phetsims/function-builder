43642d2ec1bec6e132e5eccc8f5143b25e9ceebb
pixelzoom <cmalley@pixelzoom.com>
2023-08-23 09:41:29 -0400

    convert to TypeScript

--- js/common/model/cards/Card.ts
+++ js/common/model/cards/Card.ts
@@ -1,27 +1,29 @@
 // Copyright 2015-2023, University of Colorado Boulder
 
 /**
- * Base class for cards. Other than providing 'input' information to the builder, cards have no responsibility
- * for what is displayed on them. That is handled by CardNode and its subtypes.
+ * Card is the base class for cards. Other than providing 'input' information to the builder, cards have no
+ * responsibility for what is displayed on them. That is handled by CardNode and its subtypes.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../../phet-core/js/merge.js';
 import functionBuilder from '../../../functionBuilder.js';
 import FBConstants from '../../FBConstants.js';
-import FBMovable from '../FBMovable.js';
+import FBMovable, { FBMovableOptions } from '../FBMovable.js';
+import optionize, { EmptySelfOptions } from '../../../../../phet-core/js/optionize.js';
+
+type SelfOptions = EmptySelfOptions;
+export type CardOptions = SelfOptions & FBMovableOptions;
 
 export default class Card extends FBMovable {
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
+  protected constructor( providedOptions?: CardOptions ) {
 
-    options = merge( {
-      animationSpeed: FBConstants.CARD_ANIMATION_SPEED  // {number} distance/second when animating
-    }, options );
+    const options = optionize<CardOptions, SelfOptions, FBMovableOptions>()( {
+
+      // FBMovableOptions
+      animationSpeed: FBConstants.CARD_ANIMATION_SPEED
+    }, providedOptions );
 
     super( options );
   }
