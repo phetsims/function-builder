cce54c91f11ba28003fa734479b9767926047cf0
Sam Reid <reids@colorado.edu>
2024-09-25 19:32:30 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * ESlint configuration for function-builder.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import parent from '../chipper/eslint/sim.eslint.config.mjs';
+
+export default [
+  ...parent,
+  {
+    languageOptions: {
+      globals: {
+        bigRat: 'readonly'
+      }
+    }
+  },
+  {
+    files: [
+      '**/*.ts'
+    ],
+    rules: {
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-expect-error': 'allow-with-description',
+          'ts-ignore': true,
+          'ts-check': true,
+          'ts-nocheck': true
+        }
+      ]
+    }
+  }
+];
