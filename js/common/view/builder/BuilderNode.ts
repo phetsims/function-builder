e93df2c92d29dfd9b43b54a160ce90231e0667ac
Jonathan Olson <jonathan.olson@colorado.edu>
2025-08-27 19:35:47 -0600

    Only one export (named) for TReadOnlyProperty, see https://github.com/scenerystack/scenerystack/issues/55

--- js/common/view/builder/BuilderNode.ts
+++ js/common/view/builder/BuilderNode.ts
@@ -12,7 +12,7 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import TReadOnlyProperty from '../../../../../axon/js/TReadOnlyProperty.js';
+import { TReadOnlyProperty } from '../../../../../axon/js/TReadOnlyProperty.js';
 import Shape from '../../../../../kite/js/Shape.js';
 import merge from '../../../../../phet-core/js/merge.js';
 import optionize from '../../../../../phet-core/js/optionize.js';
--- js/patterns/model/functions/MysteryImageFunction.ts
+++ js/patterns/model/functions/MysteryImageFunction.ts
@@ -6,7 +6,7 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import TReadOnlyProperty from '../../../../../axon/js/TReadOnlyProperty.js';
+import { TReadOnlyProperty } from '../../../../../axon/js/TReadOnlyProperty.js';
 import { EmptySelfOptions } from '../../../../../phet-core/js/optionize.js';
 import RichText from '../../../../../scenery/js/nodes/RichText.js';
 import FBConstants from '../../../common/FBConstants.js';
